f4b095f274f687bcbd75c3f4f2060dfa88250023
heectorr23 <ejemplo1@gmail.com>
2024-02-14 20:26:46 +0100

    v5

--- services/login_action.php
+++ services/login_action.php
@@ -1,20 +1,24 @@
 <?php
-$nombre=$_POST['nombre'];
-$correo=$_POST['correo'];
-$contrasena=$_POST['contrasena'];
-$conexion= new PDO('mysql:host=localhost;dbname=hito1PG','root','');
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$contrasena = $_POST['contrasena'];
+$conexion = new PDO('mysql:host=localhost;dbname=hito1PG','root','');
 
-$sql = "SELECT nombre, email, contrasena FROM usuarios WHERE email = :correo";
+$sql = "SELECT nombre, email, contrasena FROM usuarios WHERE nombre = :nombre AND email = :correo";
 
 $stmt = $conexion->prepare($sql);
-$stmt->bindParam(':correo',$correo);
+$stmt->bindParam(':nombre', $nombre);
+$stmt->bindParam(':correo', $correo);
 $stmt->execute();
 
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if ($result) {
-
-    // Verificar si la contraseña coincide utilizando password_verify
+if (!$result) {
+    // El nombre o correo electrónico no existen
+    echo '<p>Nombre o correo electrónico incorrectos</p>';
+    echo '<a href="../web/register.php">Registrarse</a>';
+} else {
+    // El nombre y correo electrónico existen, verificar la contraseña
     if (password_verify($contrasena, $result['contrasena'])) {
         // Iniciar la sesión
         session_start();
@@ -27,7 +31,9 @@ if ($result) {
         // Redirigir al usuario a la página del blog
         header('Location:../web/blog.php');
     } else {
-        echo '<p>Usuario no válido</p>';
-        echo '<a href="../web/register.php">Alta de usuarios</a>';
-    }//cierra else
+        // La contraseña no coincide
+        echo '<p>Contraseña incorrecta</p>';
+        echo '<a href="../web/login.php">Intentar de nuevo</a>';
+    }
 }
+?>
